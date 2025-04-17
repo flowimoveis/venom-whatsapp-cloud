b6a6718d9385ddf14c2cf4de5c20c6879714ed7b
flowimoveis <eric@flowimoveis.imb.br>
2025-04-17 12:00:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -62,5 +62,5 @@ app.post('/send', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(` Servidor rodando na porta ${PORT}`);
 });
